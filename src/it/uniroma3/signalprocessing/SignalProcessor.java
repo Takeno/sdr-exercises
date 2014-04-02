14fcfa64b68937f5a4580766ea689152b1883166
Marco Monteleone <mar.monteleone@gmail.com>
2014-04-02 19:02:22 +0200

    nuovi sinc e lowpassfilter

---
+++ src/it/uniroma3/signalprocessing/SignalProcessor.java
@@ -0,0 +1,97 @@
+package it.uniroma3.signalprocessing;
+
+import it.uniroma3.domain.*;
+
+public class SignalProcessor {
+	public static double[] convoluzione(double[] v1, double[] v2) {
+		int lunghezzaFinale = v1.length + v2.length - 1,
+			upperBound = 0,
+			lowerBound = 0,
+			k, j;
+		
+		double[] result = new double[lunghezzaFinale];
+		
+		for(k = 0; k < lunghezzaFinale; k++){
+			upperBound = Math.min(k, v2.length -1);
+			lowerBound = Math.max(0, k - v1.length + 1);
+			
+			for(j = lowerBound; j <= upperBound; j++)
+				result[k] += v1[k-j] * v2[j];
+		}
+		
+		return result;
+	}
+	
+	public static Complex[] convoluzione(Complex[] v1, Complex[] v2) {
+		int lunghezzaFinale = v1.length + v2.length - 1,
+			upperBound = 0,
+			lowerBound = 0,
+			k, j;
+		
+		Complex[] result = new Complex[lunghezzaFinale];
+		
+		for(k = 0; k < lunghezzaFinale; k++){
+			upperBound = Math.min(k, v2.length -1);
+			lowerBound = Math.max(0, k - v1.length + 1);
+			result[k] = new Complex(0, 0);
+			for(j = lowerBound; j <= upperBound; j++)
+				result[k] = result[k].somma(v1[k-j].prodotto(v2[j]));
+		}
+		
+		return result;
+	}
+	
+	public static double sinc(double n, double band){
+		if(n == 0.0)
+			return 1;
+		double range = 1.0/(2.0*band);
+		if (n%range == 0.0)
+			return 0;
+		
+		double sincArgument = Math.PI * band * n;
+		return Math.sin(sincArgument) / sincArgument;
+	}
+	
+	/**
+	 * Crea un nuovo segnale rappresentante il filtro passa-basso
+	 * NOTA BENE: il numero di campioni che deve essere passato deve essere dispari
+	 * Ottimizzare il metodo come richiesto nell'homework
+	 * 
+	 * @param band
+	 * @param numCampioni
+	 * @return Segnale discreto
+	 */
+	
+	
+	
+	public static Signal lowPassFilter(double band) {
+		int numCampioni =  ((int)(1.0 / (2.0*band)))*2 +1;
+		
+		Complex[] values = new Complex[numCampioni];
+		int simmetria = numCampioni / 2;
+		
+		for(int n = -simmetria; n <= simmetria; n++){
+			double realval = 2 * band * sinc(n, band);
+			values[n + simmetria] = new Complex(realval, 0);
+		}
+		Signal lpf = new Signal(values);
+		return lpf;
+	}
+	
+	/**
+	 * Operazione di convoluzione fra segnali:
+	 * implementa un'operazione di convoluzione discreta fra due segnali passati come parametro.
+	 * Presuppone che il segnale d'ingresso abbia parte reale e immaginaria non nulle 
+	 * e che il filtro abbia solo parte reale.
+	 * @param segnaleIn
+	 * @param rispImpulsivaFiltro
+	 * @return
+	 */
+	public static Signal convoluzione(Signal segnaleIn, Signal rispImpulsivaFiltro){
+		
+		Complex[] values = convoluzione(segnaleIn.getValues(), rispImpulsivaFiltro.getValues());
+		Signal signal = new Signal(values);
+		
+		return signal;
+	}
+}
---
+++ tests/it/uniroma3/signalprocessing/SignalProcessorTest.java
@@ -0,0 +1,79 @@
+package it.uniroma3.signalprocessing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+import it.uniroma3.domain.*;
+
+public class SignalProcessorTest {
+	
+	private double[] vettoreRealeUno;
+	private double[] vettoreRealeDue;
+	
+	private Complex[] vettoreComplessoUno;
+	private Complex[] vettoreComplessoDue;
+	
+	private Signal filtroPassaBasso;
+
+	@Before
+	public void setUp() throws Exception {
+		this.vettoreRealeUno = new double[] {3,2,1};
+		this.vettoreRealeDue = new double[] {1,1,2,1};
+		
+		this.vettoreComplessoUno = new Complex[] {
+			new Complex(3,0),
+			new Complex(2,0),
+			new Complex(1,0)
+		};
+		
+		this.vettoreComplessoDue = new Complex[] {
+			new Complex(1,0),
+			new Complex(1,0),
+			new Complex(2,0),
+			new Complex(1,0)
+		};
+	}
+
+	
+	@Test
+	public void convoluzioneReale() {
+		double[] vettoreRisultato = SignalProcessor.convoluzione(this.vettoreRealeUno, this.vettoreRealeDue);
+		
+		assertEquals(3, vettoreRisultato[0], 0);
+		assertEquals(5, vettoreRisultato[1], 0);
+		assertEquals(9, vettoreRisultato[2], 0);
+		assertEquals(8, vettoreRisultato[3], 0);
+		assertEquals(4, vettoreRisultato[4], 0);
+		assertEquals(1, vettoreRisultato[5], 0);
+	}
+	
+	@Test
+	public void convoluzioneComplessa() {
+		Complex[] vettoreRisultato = SignalProcessor.convoluzione(this.vettoreComplessoUno, this.vettoreComplessoDue);
+		
+		assertEquals(3, vettoreRisultato[0].getReale(), 0);
+		assertEquals(5, vettoreRisultato[1].getReale(), 0);
+		assertEquals(9, vettoreRisultato[2].getReale(), 0);
+		assertEquals(8, vettoreRisultato[3].getReale(), 0);
+		assertEquals(4, vettoreRisultato[4].getReale(), 0);
+		assertEquals(1, vettoreRisultato[5].getReale(), 0);
+	}
+	
+	@Test
+	public void sinc() {
+		double band = 0.25;
+		assertEquals(1, SignalProcessor.sinc(0, 20), 0);
+		assertEquals(0, SignalProcessor.sinc(1.0/(2.0*band), band), 0);
+		System.out.println(SignalProcessor.sinc(0.42, band));
+		assertEquals(0.9819, SignalProcessor.sinc(0.42, band), 0.0001);
+	}
+	
+	@Test
+	public void costruzioneFiltroPassaBasso() {
+		Signal filter = SignalProcessor.lowPassFilter(0.25);
+		for(Complex c : filter.getValues())
+			System.out.println(c);
+//		assertEquals()
+	}
+}
